3ee6270425d5ef4739cbe60792891d32fde1521d
narahma2 <rahmann@purdue.edu>
2020-03-18 16:45:42 -0500

    Update temperature_etoh_tablescan_ij_summary.py

--- temperature/APS 2017-2/temperature_etoh_tablescan_ij_summary.py
+++ temperature/APS 2017-2/temperature_etoh_tablescan_ij_summary.py
@@ -32,8 +32,21 @@ for fld in flds:
 	temp = fld.rsplit('/')[-1]
 	files = glob.glob(fld + '/Profiles/profile*')
 	names = [x.rsplit('/')[-1].rsplit('_')[-1].rsplit('.')[0] for x in files]
-	df = pd.DataFrame(columns=['Mean','StD','CV'], index=names)
+	df = pd.DataFrame(columns=['Mean', 'StD', 'CfVar', 'CfDisp'], index=names)
 	for name, file in zip(names, files):
 		data = np.loadtxt(file)
-		df.loc[name] = pd.Series({'Mean': round(np.mean(data), 3), 'StD': round(np.std(data), 3), 'CV': round(np.std(data) / np.mean(data), 3)})
+
+		# Calculate Coefficient of Variation
+		mean = np.mean(data)
+		std = np.std(data)
+		cv = np.std(data) / np.mean(data)
+
+		# Calculate Coefficient of Dispersion
+		Q1 = np.percentile(data, 25, interpolation = 'midpoint')
+		Q3 = np.percentile(data, 75, interpolation = 'midpoint')
+		cd = (Q1 - Q3) / (Q1 + Q3)
+
+		# Fill in specific profile in DataFrame
+		df.loc[name] = pd.Series({'Mean': round(mean, 3), 'StD': round(std, 3), 'CfVar': round(cv, 3), 'CfDisp': round(cd, 3)})
+		
 	df.to_csv(fld + '/' + temp + 'summary.txt', sep='\t')
