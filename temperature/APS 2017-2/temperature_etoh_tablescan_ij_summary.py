42545e93049824cdd3159db739842d860a27d0f7
narahma2 <rahmann@purdue.edu>
2020-03-18 11:57:56 -0500

    Create IJ ramping summary script

---
+++ temperature/APS 2017-2/temperature_etoh_tablescan_ij_summary.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Summarizes the processed IJ Ramping data sets.
+See "X-ray Temperature/APS 2017-2/IJ Ethanol Ramping" in OneNote.
+
+Created on Wedn March  18 11:55:00 2020
+
+@author: rahmann
+"""
+
+import sys
+if sys.platform == 'win32':
+	sys.path.append('E:/GitHub/xray/general')
+	sys.path.append('E:/GitHub/xray/temperature')
+	sys_folder = 'R:'
+elif sys.platform == 'linux':
+	sys.path.append('/mnt/e/GitHub/xray/general')
+	sys.path.append('/mnt/e/GitHub/xray/temperature')
+	sys_folder = '/mnt/r/'
+
+import glob
+import numpy as np
+import pandas as pd
+
+project_folder = sys_folder + '/X-ray Temperature/APS 2017-2'
+folder = project_folder + '/Processed/Ethanol'
+
+# Create summary of Temperature data sets (to find most consistent profile)
+flds = glob.glob(folder + '/IJ Ramping/Temperature/*/')
+
+for fld in flds:
+	temp = fld.rsplit('/')[-1]
+	files = glob.glob(fld + '/Profiles/profile*')
+	names = [x.rsplit('/')[-1].rsplit('_')[-1].rsplit('.')[0] for x in files]
+	df = pd.DataFrame(columns=['Mean','StDev','CV'], index=names)
+	for name, file in zip(names, files):
+		data = np.loadtxt(file)
+		df.loc[name] = pandas.Series({'Mean': np.mean(data), 'StDev': np.std(data), 'CV': np.std(data) / np.mean(data)})
