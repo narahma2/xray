ded113e9536128322d88ca0b9463d217146e20e7
narahma2 <rahmann@purdue.edu>
2020-03-18 19:13:51 -0500

    Add position summary

--- temperature/APS 2017-2/temperature_etoh_tablescan_ij_summary.py
+++ temperature/APS 2017-2/temperature_etoh_tablescan_ij_summary.py
@@ -21,6 +21,7 @@ elif sys.platform == 'linux':
 import glob
 import numpy as np
 import pandas as pd
+from Statistics.calc_statistics import polyfit
 
 project_folder = sys_folder + '/X-ray Temperature/APS 2017-2'
 folder = project_folder + '/Processed/Ethanol'
@@ -32,9 +33,16 @@ for fld in flds:
 	temp = fld.rsplit('/')[-1]
 	files = glob.glob(fld + '/Profiles/profile*')
 	names = [x.rsplit('/')[-1].rsplit('_')[-1].rsplit('.')[0] for x in files]
-	df = pd.DataFrame(columns=['Mean', 'StD', 'CfVar', 'CfDisp'], index=names)
+	df = pd.DataFrame(columns=['R^2', 'Mean', 'StD', 'CfVar', 'CfDisp'], index=names)
 	for name, file in zip(names, files):
+		# Profile data
 		data = np.loadtxt(file)
+
+		# Y positions
+		y = np.loadtxt(fld + '/positions.txt')
+
+		# Calculate R^2
+		r2 = polyfit(data, y, 1)['determination']
 
 		# Calculate Coefficient of Variation
 		mean = np.mean(data)
@@ -47,6 +55,39 @@ for fld in flds:
 		cd = (Q1 - Q3) / (Q1 + Q3)
 
 		# Fill in specific profile in DataFrame
-		df.loc[name] = pd.Series({'Mean': round(mean, 3), 'StD': round(std, 3), 'CfVar': round(cv, 3), 'CfDisp': round(cd, 3)})
-		
+		df.loc[name] = pd.Series({'R^2': round(r2, 3), 'Mean': round(mean, 3), 'StD': round(std, 3), 'CfVar': round(cv, 3), 'CfDisp': round(cd, 3)})
+
 	df.to_csv(fld + '/' + temp + 'summary.txt', sep='\t')
+
+# Create summary of Profile data sets (to find best profile)
+flds = glob.glob(folder + '/IJ Ramping/Positions/*/')
+
+for fld in flds:
+	pos = fld.rsplit('/')[-1]
+	files = glob.glob(fld + '/Profiles/profile*')
+	names = [x.rsplit('/')[-1].rsplit('_')[-1].rsplit('.')[0] for x in files]
+	df = pd.DataFrame(columns=['R^2', 'Mean', 'StD', 'R2', 'CfDisp'], index=names)
+	for name, file in zip(names, files):
+		# Profile data
+		data = np.loadtxt(file)
+
+		# Temperature
+		T = np.loadtxt(fld + '/temperature.txt')
+
+		# Calculate R^2
+		r2 = polyfit(data, T, 1)['determination']
+
+		# Calculate Coefficient of Variation
+		mean = np.mean(data)
+		std = np.std(data)
+		cv = np.std(data) / np.mean(data)
+
+		# Calculate Coefficient of Dispersion
+		Q1 = np.percentile(data, 25, interpolation = 'midpoint')
+		Q3 = np.percentile(data, 75, interpolation = 'midpoint')
+		cd = (Q1 - Q3) / (Q1 + Q3)
+
+		# Fill in specific profile in DataFrame
+		df.loc[name] = pd.Series({'R^2': round(r2, 3), 'Mean': round(mean, 3), 'StD': round(std, 3), 'CfVar': round(cv, 3), 'CfDisp': round(cd, 3)})
+
+	df.to_csv(fld + '/' + pos + 'summary.txt', sep='\t')
