f22c554a835dbf16795569b1beb98963d68f4fc2
rahmann@purdue.edu <rahmann@purdue.edu>
2020-03-23 17:53:43 -0400

    Cleaned up code - Removed maximum normalization. - Removed extraneous profiles & fits. - Cleaned up plots.

--- temperature/temperature_processing.py
+++ temperature/temperature_processing.py
@@ -61,7 +61,7 @@ def profile(name, fit_var, profile, profiles_folder, stats_folder, test, plots_f
     
     plt.figure()
     plt.plot(profile, fit_var, ' o', markerfacecolor='none', markeredgecolor='b', label='Data')
-    plt.plot(profile, profile_polyfit['function'](profile), 'k', linewidth=2.0, label='y = ' + '%0.2f'%profile_polyfit['polynomial'][0] + ' + ' + '%0.2f'%profile_polyfit['polynomial'][1])
+    plt.plot(profile, profile_polyfit['function'](profile), 'k', linewidth=2.0, label='y = ' + '%0.2f'%profile_polyfit['polynomial'][0] + 'x + ' + '%0.2f'%profile_polyfit['polynomial'][1])
     plt.title(test + ' ' + name + ' - R$^2$ = ' + str(round(profile_polyfit['determination'],4)))
     plt.legend()
     if 'IJ Ramping/Temperature' in profiles_folder:
@@ -131,90 +131,22 @@ def main(test, folder, scan, reduced_intensity, reduced_q, temperature=None, str
     if not os.path.exists(curves_folder):
         os.makedirs(curves_folder)
         
-    concavity_folder = folder + '/' + str(scan) + '/Concavity/'
-    if not os.path.exists(concavity_folder):
-        os.makedirs(concavity_folder)
-    
     if structure_factor:
         structure_factor_folder = folder + '/' + str(scan) + '/Structure Factor/'
         if not os.path.exists(structure_factor_folder):
             os.makedirs(structure_factor_folder)
     
-    reduced_intensity = np.array([x / np.max(x) for x in reduced_intensity])
+    #reduced_intensity = np.array([x / np.max(x) for x in reduced_intensity])
     
-    concavity = [-np.gradient(np.gradient(i)) for i in reduced_intensity]
-    concavity = [savgol_filter(j, 55, 3) for j in concavity]
-    conc_peak_locs = [find_peaks(k, height=0.00001, distance=100, width=15)[0] for k in concavity]
-    conc_q1 = np.array([reduced_q[x[0]] for x in conc_peak_locs])
-    conc_q2 = np.array([reduced_q[x[1]] if len(x) == 2 else np.nan for x in conc_peak_locs])   
-
-    #%% Gaussian fitting
-    peak_q = [reduced_q[np.argmax(x)] for x in reduced_intensity]
-    peak = [np.max(x) for x in reduced_intensity]
-    
-    # Expected/Bounds: [mu1,sigma1,A1,mu2,sigma2,A2]
-    # mu:       location of peak
-    # sigma:    width of peak
-    # A:        height of peak
-    
-    if 'Water' in test:
-        expected = [(peak_q[i],0.4,peak[i],  2.7,0.3,0.5) for i,_ in enumerate(peak)]
-        bounds = [((peak_q[i]-1E-2,-np.inf,peak[i]-1E-2,  2.5,0.2,0.4), (peak_q[i],np.inf,peak[i],  3.3,0.8,0.7)) for i,_ in enumerate(peak)]
-    elif 'Ethanol' in test:
-        expected = [(0.7,0.3,0.3,  peak_q[i],0.25,peak[i]) for i,_ in enumerate(peak)]
-        bounds = [((0.5,0.2,0.3,  peak_q[i]-1E-6,-np.inf,peak[i]-1E-6), (0.9,0.4,0.45,  peak_q[i]+1E-6,np.inf,peak[i]+1E-6)) for i,_ in enumerate(peak)]
-    elif 'Dodecane' in test:
-        expected = [(peak_q[i],0.3,peak[i]) for i,_ in enumerate(peak)]
-        bounds = [((peak_q[i]-1E-6,-np.inf,peak[i]-1E-6), (peak_q[i]+1E-6,np.inf,peak[i]+1E-6)) for i,_ in enumerate(peak)]
-    
-    if 'Dodecane' not in test:
-        fits = [curve_fit(bimodal,reduced_q,x,expected[i],bounds=bounds[i]) for i,x in enumerate(reduced_intensity)]
-    elif 'Dodecane' in test:
-        fits = [curve_fit(gauss,reduced_q,x,expected[i],bounds=bounds[i]) for i,x in enumerate(reduced_intensity)]
-    params = [x[0] for x in fits]
-    cov = [x[1] for x in fits]
-    sigma = [np.sqrt(np.diag(x)) for x in cov]
-    
-    mu1 = np.array([x[0] for x in params])
-    mu1_int = np.array([reduced_intensity[i][np.abs(x - reduced_q).argmin()] for i,x in enumerate(mu1)])
-    sigma1 = np.array([x[1] for x in params])
-    A1 = np.array([x[2] for x in params])
-    
-    if 'Dodecane' not in test:
-        mu2 = np.array([x[3] for x in params])
-        mu2_int = np.array([reduced_intensity[i][np.abs(x - reduced_q).argmin()] for i,x in enumerate(mu2)])
-        sigma2 = np.array([x[4] for x in params])
-        A2 = np.array([x[5] for x in params])
-    else:
-        mu2 = np.zeros(mu1.shape)
-        mu2_int = np.zeros(mu1.shape)
-        sigma2 = np.zeros(mu1.shape)
-        A2 = np.zeros(mu1.shape)
-    
-    #%% Find pinned points in the curves (least variation)
+    ## Find pinned points in the curves (least variation)
     intensity_std = np.std(reduced_intensity, axis=0)
     pinned_pts = find_peaks(-intensity_std)[0]
-    pinned_q = [reduced_q[int(x)] for x in pinned_pts]
-    pinned_q_int1 = [x*0 for x in mu1_int] #np.array([reduced_intensity[i][int(x)] for i,x in enumerate(pinned_pts)][0])
-    pinned_q_int2 = [x*0 for x in mu2_int] #np.array([reduced_intensity[i][int(x)] for i,x in enumerate(pinned_pts)][-1])
+    # Find the minimum peak only (throw away every other valley)
+    pinned_pts = pinned_pts[np.argmin(intensity_std[pinned_pts])]
+    pinned_q = reduced_q[pinned_pts] #[reduced_q[int(x)] for x in pinned_pts]
     peak_pts = find_peaks(intensity_std)[0]
-    peak_q = [reduced_q[int(x)] for x in peak_pts]
-    
-    #%% Polynomial fitting
-    poly_sigma = np.ones(len(reduced_q))
-#    poly_sigma[pinned_pts[-1]] = 0.01    
-    fits = [curve_fit(poly4,reduced_q,x,sigma=poly_sigma) for x in reduced_intensity]
-    params = [x[0] for x in fits]
-    
-    c0 = np.array([x[0] for x in params])
-    c1 = np.array([x[1] for x in params])
-    c2 = np.array([x[2] for x in params])
-    c3 = np.array([x[3] for x in params])
-    c4 = np.array([x[4] for x in params])
-    
-    intensity_poly4 = [c0[i]*reduced_q**4 + c1[i]*reduced_q**3 + c2[i]*reduced_q**2 + c3[i]*reduced_q + c4[i] for i,_ in enumerate(c0)]
+    #peak_q = [reduced_q[int(x)] for x in peak_pts]
 
-    #%%
     if y is not None:
         fit_var = y
         np.savetxt(profiles_folder + '/positions.txt', y)
@@ -226,37 +158,13 @@ def main(test, folder, scan, reduced_intensity, reduced_q, temperature=None, str
     
     profile('peak', fit_var, [np.max(x) for x in reduced_intensity], profiles_folder, stats_folder, test, plots_folder)
     profile('peakq', fit_var, [reduced_q[np.argmax(x)] for x in reduced_intensity], profiles_folder, stats_folder, test, plots_folder)
-    profile('q1', fit_var, mu1, profiles_folder, stats_folder, test, plots_folder)
-    profile('q1int', fit_var, mu1_int, profiles_folder, stats_folder, test, plots_folder)
-    profile('q2', fit_var, mu2, profiles_folder, stats_folder, test, plots_folder)
-    profile('q2int', fit_var, mu2_int, profiles_folder, stats_folder, test, plots_folder)
-    profile('qratio', fit_var, mu1 / mu2, profiles_folder, stats_folder, test, plots_folder)
-    profile('qdist', fit_var, mu1 - mu2, profiles_folder, stats_folder, test, plots_folder)
-    profile('iratio', fit_var, mu1_int / mu2_int, profiles_folder, stats_folder, test, plots_folder)
-    #profile('ipratio', fit_var, mu1_int / pinned_q_int2, profiles_folder, stats_folder, test, plots_folder)
-    profile('ratio', fit_var, (mu1_int/mu2_int)/(mu1/mu2), profiles_folder, stats_folder, test, plots_folder)
-    profile('aratio', fit_var, [np.trapz(x[:pinned_pts[-1]], reduced_q[:pinned_pts[-1]]) / np.trapz(x[pinned_pts[-1]:], reduced_q[pinned_pts[-1]:]) for x in reduced_intensity], profiles_folder, stats_folder, test, plots_folder)
+    profile('aratio', fit_var, [np.trapz(x[:pinned_pts], reduced_q[:pinned_pts]) / np.trapz(x[pinned_pts:], reduced_q[pinned_pts:]) for x in reduced_intensity], profiles_folder, stats_folder, test, plots_folder)
     profile('mean', fit_var, [np.mean(x) for x in reduced_intensity], profiles_folder, stats_folder, test, plots_folder)
     profile('var', fit_var, [np.var(x) for x in reduced_intensity], profiles_folder, stats_folder, test, plots_folder)
     profile('skew', fit_var, [stats.skew(x) for x in reduced_intensity], profiles_folder, stats_folder, test, plots_folder)
     profile('kurt', fit_var, [stats.kurtosis(x) for x in reduced_intensity], profiles_folder, stats_folder, test, plots_folder)
     
-    # Gaussian parameters
-    profile('sigma1', fit_var, sigma1, profiles_folder, stats_folder, test, plots_folder)
-    profile('sigma2', fit_var, sigma2, profiles_folder, stats_folder, test, plots_folder)
-    profile('A1', fit_var, A1, profiles_folder, stats_folder, test, plots_folder)
-    profile('A2', fit_var, A2, profiles_folder, stats_folder, test, plots_folder)
-    
-    # Polynomial parameters
-    profile('c0', fit_var, c0, profiles_folder, stats_folder, test, plots_folder)
-    profile('c1', fit_var, c1, profiles_folder, stats_folder, test, plots_folder)
-    profile('c2', fit_var, c2, profiles_folder, stats_folder, test, plots_folder)
-    profile('c3', fit_var, c3, profiles_folder, stats_folder, test, plots_folder)
-    profile('c4', fit_var, c4, profiles_folder, stats_folder, test, plots_folder)
-
     profile_peakq = np.loadtxt(profiles_folder + '/profile_peakq.txt')
-    profile_q1 = np.loadtxt(profiles_folder + '/profile_q1.txt')
-    profile_q2 = np.loadtxt(profiles_folder + '/profile_q2.txt')
 
     rr = np.array([(x-min(fit_var))/(max(fit_var)-min(fit_var)) for x in fit_var])
     bb = np.array([1-(x-min(fit_var))/(max(fit_var)-min(fit_var)) for x in fit_var])
@@ -279,15 +187,12 @@ def main(test, folder, scan, reduced_intensity, reduced_q, temperature=None, str
         # Create intensity plots with q values of interest highlighted
         plt.figure()
         plt.plot(reduced_q, reduced_intensity[i], linestyle='-', color=(rr[i],0,bb[i]), linewidth=2.0, label=str(round(fit_var[i],1)) + '°C')
-        plt.plot(reduced_q, intensity_poly4[i], linestyle='-', color='k', linewidth=1.0, label='Poly4')
         plt.axvline(x=profile_peakq[i], linestyle='--', color='C1', label='peakq = ' + str(round(profile_peakq[i],2)))
-        plt.axvline(x=profile_q1[i], linestyle='--', color='C2', label='q$_1$ = ' + str(round(profile_q1[i],2)))
-        plt.axvline(x=profile_q2[i], linestyle='--', color='C3', label='q$_2$ = ' + str(round(profile_q2[i],2)))
         plt.legend(loc='upper right')
         plt.xlabel('q (Å$^{-1}$)')
         plt.ylabel('Intensity (a.u.)')
         plt.autoscale(enable=True, axis='x', tight=True)
-        plt.gca().set_ylim([llim, 1.02])
+        #plt.gca().set_ylim([llim, 1.02])
         plt.minorticks_on()
         plt.tick_params(which='both',direction='in')
         plt.title(test + ' Curves')
@@ -297,30 +202,6 @@ def main(test, folder, scan, reduced_intensity, reduced_q, temperature=None, str
             
     
     # if IJ is False:
-    for i,_ in enumerate(concavity):
-        if temperature is not None:
-            np.savetxt(tests_folder + '/concavity' + f"{i:02d}" + '_' + str(round(fit_var[i],2)).replace('.','p') + 'C.txt', concavity[i])
-        elif y is not None:
-            np.savetxt(tests_folder + '/concavity' + f"{i:02d}" + '_' + str(round(fit_var[i],2)).replace('.','p') + 'mm.txt', concavity[i])
-        
-        plt.figure()
-        if temperature is not None:
-            plt.plot(reduced_q, concavity[i], linestyle='-', color=(rr[i],0,bb[i]), linewidth=2.0, label=str(round(fit_var[i],1)) + '°C')
-        elif y is not None:
-            plt.plot(reduced_q, concavity[i], linestyle='-', color=(rr[i],0,bb[i]), linewidth=2.0, label=str(round(fit_var[i],1)) + 'mm')
-        plt.axvline(x=conc_q1[i], linestyle='--', color='C2', label='q$_1$ = ' + str(round(conc_q1[i],2)))
-        plt.axvline(x=conc_q2[i], linestyle='--', color='C3', label='q$_2$ = ' + str(round(conc_q2[i],2)))
-        plt.legend(loc='upper right')
-        plt.xlabel('q (Å$^{-1}$)')
-        plt.ylabel('Negative Concavity (a.u.)')
-        plt.autoscale(enable=True, axis='x', tight=True)
-        plt.minorticks_on()
-        plt.tick_params(which='both',direction='in')
-        plt.title(test + ' Curves')
-        plt.tight_layout()
-        plt.savefig(concavity_folder +  '/' + test + 'curves_' + str(round(fit_var[i],1)) + '.png')
-        plt.close()
-        
     if structure_factor:    
         for i,_ in enumerate(structure_factor):
             np.savetxt(tests_folder + '/' + f"{i:02d}" + '_' + str(round(temperature[i],2)).replace('.','p') + 'C' + '.txt', structure_factor[i])
@@ -350,10 +231,10 @@ def main(test, folder, scan, reduced_intensity, reduced_q, temperature=None, str
     plt.plot(reduced_q, intensity_std, linewidth='2.0')
     plt.xlabel('q (Å$^{-1}$)')
     plt.ylabel('SD(Intensity) [a.u.]')
-    [plt.axvline(x=y, color='k', linestyle='--') for y in pinned_q]
-    [plt.text(x, 0.6*np.mean(intensity_std), 'q = ' + "%02.2f"%round(x, 2), horizontalalignment='center', bbox=dict(facecolor='white', alpha=1.0)) for x in pinned_q]
-    [plt.axvline(x=y, color='g', linestyle='-.') for y in peak_q]
-    [plt.text(x, 1.2*np.mean(intensity_std), 'q = ' + "%02.2f"%round(x, 2), horizontalalignment='center', bbox=dict(ec='g',facecolor='white', alpha=1.0)) for x in peak_q]
+    plt.axvline(x=pinned_q, color='k', linestyle='--')
+    plt.text(pinned_q, 0.6*np.mean(intensity_std), 'q = ' + "%02.2f"%round(pinned_q, 2), horizontalalignment='center', bbox=dict(facecolor='white', alpha=1.0))
+    #[plt.axvline(x=y, color='g', linestyle='-.') for y in peak_q]
+    #[plt.text(x, 1.2*np.mean(intensity_std), 'q = ' + "%02.2f"%round(x, 2), horizontalalignment='center', bbox=dict(ec='g',facecolor='white', alpha=1.0)) for x in peak_q]
     plt.title('Scan ' + str(scan))
     plt.tight_layout()
     plt.savefig(plots_folder + 'stdev.png')
@@ -364,12 +245,10 @@ def main(test, folder, scan, reduced_intensity, reduced_q, temperature=None, str
     [plt.plot(reduced_q, x, color=(rr[i],0,bb[i])) for i,x in enumerate(reduced_intensity)]
     plt.xlabel('q (Å$^{-1}$)')
     plt.ylabel('Intensity [a.u.]')
-    [plt.axvline(x=y, color='k', linestyle='--') for y in pinned_q]
-    [plt.text(x, 0.5, 'q = ' + "%02.2f"%round(x, 2), horizontalalignment='center', bbox=dict(facecolor='white', alpha=1.0)) for x in pinned_q]
-    [plt.axvline(x=y, color='g', linestyle='-.') for y in peak_q]
-    [plt.text(x, 0.7, 'q = ' + "%02.2f"%round(x, 2), horizontalalignment='center', bbox=dict(ec='g',facecolor='white', alpha=1.0)) for x in peak_q]
+    plt.axvline(x=pinned_q, color='k', linestyle='--')
+    plt.text(pinned_q, 0.5, 'q = ' + "%02.2f"%round(pinned_q, 2), horizontalalignment='center', bbox=dict(facecolor='white', alpha=1.0))
     plt.title('Scan ' + str(scan))
-    plt.ylim([llim, 1.05])
+    #plt.ylim([llim, 1.05])
     plt.tight_layout()
     plt.savefig(plots_folder + 'superimposedcurves.png')
     #    plt.close()
