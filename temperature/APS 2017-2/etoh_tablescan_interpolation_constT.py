305f5cc2d0ce328686e9e0d56c28ee70e1c25c58
Naveed (archHarbinger) <rahmann@purdue.edu>
2020-04-07 13:14:29 -0500

    Initial commit

---
+++ temperature/APS 2017-2/etoh_tablescan_interpolation_constT.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Compares the interpolated IJ Ramping temperatures from the calibration to the Nozzle T.
+This script tests how much the temperature changes at the different positions.
+
+@Author: naveed
+@Date:   2020-04-07 12:40:31
+@Last Modified by:   naveed
+@Last Modified time: 2020-04-07 12:37:33
+"""
+
+mport sys
+if sys.platform == 'win32':
+	sys.path.append('E:/GitHub/xray/general')
+	sys.path.append('E:/GitHub/xray/temperature')
+	sys_folder = 'R:'
+elif sys.platform == 'linux':
+	sys.path.append('/mnt/e/GitHub/xray/general')
+	sys.path.append('/mnt/e/GitHub/xray/temperature')
+	sys_folder = '/mnt/r/'
+
+import glob
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from Statistics.calc_statistics import polyfit
+
+## Setup initial parameters
+project_folder = sys_folder + '/X-ray Temperature/APS 2017-2'
+folder = project_folder + '/Processed/Ethanol'
+
+# Select profiles to be used as thermometers
+profiles = ['aratio', 'peak', 'peakq', 'var', 'skew', 'kurt']
+
+# Select calibration data sets
+calibrations = ['408', '409', 'Combined']
+
+# Load in IJ Ramping folders
+flds = glob.glob(folder + '/IJ Ramping/Temperature/T*/')
+
+# Positions array
+positions = np.loadtxt(folder + '/IJ Ramping/Temperature/T281p62/positions.txt')
+
+## Load in and process data sets
+# Iterate through each selected calibration jet
+for calibration in calibrations:
+	# Iterate through each selected profile
+	for j, profile in enumerate(profiles):
+		# Create polynomial object based on selected profile & calibration jet
+		# Load APS 2018-1 for 'Combined'
+		if calibration == 'Combined':
+			p = np.poly1d(np.loadtxt(folder.replace('APS 2017-2', 'APS 2018-1').replace('Ethanol', 'Ethanol_700umNozzle') + '/' + calibration + '/Statistics/' + profile + '_polynomial.txt'))
+		else:
+			p = np.poly1d(np.loadtxt(folder + '/' + calibration + '/Statistics/' + profile + '_polynomial.txt'))
+
+		# Create folders
+		plots_folder = folder + '/IJ Ramping/TemperatureInterp/' + calibration + '_' + profile
+		if not os.path.exists(plots_folder):
+			os.makedirs(plots_folder)
+
+		# Load in Positions
+		T = []
+		for i, fld in enumerate(flds):
+			# Y position string (y00p25, y00p05, etc.)
+			Tp = fld.rsplit('/')[-2]
+			T.append(float(Tp[1:].replace('p','.')))
+
+			# Profile data for the IJ Ramping Positions
+			data = np.loadtxt(fld + '/Profiles/profile_' + profile + '.txt')
+
+			# Nozzle T
+			nozzleT = T[-1]
+			# Interpolated temperature
+			interpT = p(data)
+			# Fit a linear line of interpT vs. nozzleT
+			fit = polyfit(interpT, nozzleT, 1)
+
+			# Plot results
+			plt.figure()
+			plt.plot(positions, interpT, ' o', markerfacecolor='none', markeredgecolor='b', label='Data')
+			plt.title('T = ' + Tp[1:].replace('p', '.') + ' K - ' + calibration + ': ' + profile)
+			plt.legend()
+			plt.xlabel('Y Location (mm)')
+			plt.ylabel('Interpolated Temperature (K)')
+			plt.tight_layout()
+			plt.savefig(plots_folder + '/' + Tp + '.png')
+			plt.close()
