7b0fe254c12be1b1eb322a89427f3c0b0a910805
narahma2 <rahmann@purdue.edu>
2020-04-14 20:29:54 -0400

    UPDATE: Background change

--- temperature/APS 2017-2/etoh_ij_cold.py
+++ temperature/APS 2017-2/etoh_ij_cold.py
@@ -29,16 +29,18 @@ test = 'Ethanol/IJ Cold'
 folder = project_folder + '/Processed/Ethanol'
 
 g = h5py.File(project_folder + '/RawData/Scan_429.hdf5', 'r')
-bg = [g['Intensity_vs_q'][:,i] for i in range(np.shape(g['Intensity_vs_q'])[1])]
-bg_avg = np.mean(bg, axis=0)
 
 f = h5py.File(project_folder + '/RawData/Scan_423.hdf5', 'r')
 q = list(f['q'])
 
 # Grab intensity profiles and crop down to match len(y) (detector collected for too long?)
 intensity = [f['Intensity_vs_q'][:,i] for i in range(np.shape(f['Intensity_vs_q'])[1])]
-intensity = [(x-bg_avg) for x in intensity]
 intensity = intensity[:50]
+
+# Let the last 10 images be the background
+bg = intensity[-10:]
+bg_avg = np.mean(bg, axis=0)
+intensity = [(x-bg_avg) for x in intensity]
 
 # Grab vertical locations
 y = np.array(list(f['7bmb1:aero:m1.VAL']))
@@ -52,4 +54,4 @@ reduced_q = np.array(q[sl])
 reduced_intensity = [x[sl] for x in filtered_intensity]
 reduced_intensity = np.array([y/np.trapz(y, x=reduced_q) for y in reduced_intensity])
 
-temperature_processing(test.rsplit('/')[0], folder, test.rsplit('/')[1], reduced_intensity, reduced_q, temperature=[], structure_factor=None, y=y, ramping=False, scatter=f['Scatter_images'][:50], background=g['Scatter_images'][:50])
+temperature_processing(test.rsplit('/')[0], folder, test.rsplit('/')[1], reduced_intensity[:-15], reduced_q, temperature=[], structure_factor=None, y=y[:-15], ramping=False)
--- temperature/APS 2017-2/etoh_tablescan_ij.py
+++ temperature/APS 2017-2/etoh_tablescan_ij.py
@@ -19,10 +19,12 @@ elif sys.platform == 'linux':
 	sys_folder = '/mnt/r/'
 
 import os
+import glob
 import h5py
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import pickle
 from scipy import stats
 from scipy.constants import convert_temperature
 from scipy.signal import savgol_filter, find_peaks
@@ -35,10 +37,8 @@ rank_x = np.linspace(0,13, num=14, dtype=int)
 project_folder = sys_folder + '/X-ray Temperature/APS 2017-2'
 folder = project_folder + '/Processed/Ethanol'
 
+# Load in intensities
 f = h5py.File(project_folder + '/RawData/Scan_' + str(425) + '.hdf5', 'r')
-g = h5py.File(project_folder + '/RawData/Scan_' + str(429) + '.hdf5', 'r')
-bg = [g['Intensity_vs_q'][:,i] for i in range(np.shape(g['Intensity_vs_q'])[1])]
-bg_avg = np.mean(bg, axis=0)
 
 q = list(f['q'])
 intensities = [f['Intensity_vs_q'][:,i] for i in range(np.shape(f['Intensity_vs_q'])[1])]
@@ -63,10 +63,16 @@ for k in y_locs:
 	positions.append([i for i, x in enumerate(y_loc) if x == round(k, 2)])      # 14 total occurrences of the 18 y positions        
 
 # Read only the first 252 intensity scans (there's 277, seems to be extraneous data?)
-intensities = intensities[0:252]
+intensities = np.array(intensities[0:252])
 
-# Background subtraction
-intensities = [(x-bg_avg) for x in intensities]
+# Load in background
+g = h5py.File(project_folder + '/RawData/Scan_' + str(429) + '.hdf5', 'r')
+
+for x in positions[0]:
+	# Make the y = 10.0 mm position the background
+	bg = intensities[x+17]
+	# Background subtraction
+	intensities[x:(x+18)] = intensities[x:(x+18)] - bg
 
 # Smooth out data using Savitzky-Golay filter
 filtered_intensity = [savgol_filter(x, 55, 3) for x in intensities]
@@ -81,14 +87,54 @@ reduced_intensity = [y/np.trapz(y, x=reduced_q) for y in reduced_intensity]
 # Convert reduced_intensity to numpy array
 reduced_intensity = np.array(reduced_intensity)
 
-# Constant temperature plots (variable position)
-for x, i in enumerate(positions[0]):
-	# Crop out the points before impingment (first 7)
-	y = y_loc[18*x+7:18*(x+1)]
-	temperature = np.mean(nozzle_T[18*x+7:18*(x+1)])
-	temperature_processing(test.rsplit('/')[0], folder, test.rsplit('/')[1] + '/Temperature/T' + '{0:05.2f}'.format(temperature).replace('.', 'p'), reduced_intensity[18*x+7:18*(x+1)], reduced_q, temperature=None, structure_factor=None, y=y, ramping=True, scatter=f['Scatter_images'][18*x+7:18*(x+1)], background=g['Scatter_images'])
+# # Constant temperature plots (variable position)
+for x in positions[0]:
+# 	# Crop out y = 9.0 and 10.0 mm points
+	y = y_loc[x:(x+16)]
+	temperature = np.mean(nozzle_T[x:(x+16)])
+	temperature_processing(test.rsplit('/')[0], folder, test.rsplit('/')[1] + '/Temperature/T' + '{0:05.2f}'.format(temperature).replace('.', 'p'), reduced_intensity[x:(x+16)], reduced_q, temperature=None, structure_factor=None, y=y, ramping=True, scatter=f['Scatter_images'][x:(x+16)], background=g['Scatter_images'])
 
 # Constant position plots (variable temperature)
-for x, i in enumerate(positions):
+for x, i in enumerate(positions[:-1]):
 	y = y_locs[x]
-	temperature_processing(test.rsplit('/')[0], folder, test.rsplit('/')[1] + '/Positions/y' + '{0:05.2f}'.format(y).replace('.', 'p'), reduced_intensity[i], reduced_q, temperature=nozzle_T[i], structure_factor=None, y=None, ramping=True)
+	temperature_processing(test.rsplit('/')[0], folder, test.rsplit('/')[1] + '/Positions/y' + '{0:05.2f}'.format(y).replace('.', 'p'), reduced_intensity[i], reduced_q, temperature=nozzle_T[i], structure_factor=None, y=None, ramping=True, scatter=f['Scatter_images'][i], background=g['Scatter_images'])
+
+# Full 2D pooled case
+temperature_processing(test.rsplit('/')[0], folder, test.rsplit('/')[1] + '/Pooled/', reduced_intensity, reduced_q, temperature=nozzle_T, y=np.reshape(positions, len(reduced_intensity)), ramping=True, pooled=True)
+
+# Plot calibration jets for each of the constant T cases
+# Load calibration data set
+with open(glob.glob(project_folder + '/Processed/Ethanol/409/*.pckl')[0], 'rb') as f:
+	temperature_cal, reduced_q_cal, reduced_intensity_cal = pickle.load(f)
+
+flds = glob.glob(folder + '/IJ Ramping/Temperature/T*/')
+for fld in flds:
+	# IJ temperature
+	T = float(fld.split('/')[-2][1:].replace('p', '.'))
+
+	# Load calibration for selected IJ temperature
+	T_cal = temperature_cal[np.argmin(abs(np.array(temperature_cal) - T))]
+	intensity_cal = reduced_intensity_cal[np.argmin(abs(np.array(temperature_cal) - T))]
+
+	# Load IJ Ramping data set
+	with open(glob.glob(fld + '/*.pckl')[0], 'rb') as f:
+		temperature, y, reduced_q, reduced_intensity = pickle.load(f)
+
+	ij_y = []
+	ij_I = []
+	for i, _ in enumerate(y):
+		if not np.mod(i, 3):
+			ij_y.append(y[i])
+			ij_I.append(reduced_intensity[i])
+
+	# Plot combined scans
+	plt.figure()
+	[plt.plot(reduced_q, ij_I[i], color='C' + str(i), linestyle='--', label='y = ' + str(round(ij_y[i],2))) for i,_ in enumerate(ij_I)]
+	plt.plot(reduced_q_cal, intensity_cal, color='k', linestyle='-', label='Calib. Jet')
+	plt.title('T = ' + str(round(T)) + ' K')
+	plt.xlabel('q (Å$^{-1}$)')
+	plt.ylabel('Intensity (arb. units)')
+	plt.legend()
+	plt.tight_layout()
+	plt.savefig(fld + '/combined.png')
+	plt.close()
