f3f78d80219de209504fbd3d2de158351b36feaa
narahma2 <rahmann@purdue.edu>
2020-04-24 16:50:29 -0400

    UPDATE: Moved functions to whitebeam_2018.py

--- radiography/APS 2018-1/energy_spectra_2018_plots.py
+++ radiography/APS 2018-1/energy_spectra_2018_plots.py
@@ -28,140 +28,171 @@ from Spectra.spectrum_modeling import multi_angle as xop, xcom, xcom_reshape, de
 from scipy.interpolate import CubicSpline
 from scipy.signal import savgol_filter
 from scipy.interpolate import interp1d
-from whitebeam_2018 import spectra_angles
+from whitebeam_2018 import spectra_angles, scintillator_response2D, filtered_spectra
 
-# Location of APS 2018-1 data
-project_folder = sys_folder + '/X-ray Radiography/APS 2018-1/'
 
-# Load XOP spectra
-input_folder = project_folder + '/Spectra_Inputs'
-input_spectra = xop(input_folder + '/xsurface1.dat')
+def main():
+	# Location of APS 2018-1 data
+	project_folder = sys_folder + '/X-ray Radiography/APS 2018-1/'
 
-# Find the angles corresponding to the 2018-1 image vertical pixels
-angles_mrad = spectra_angles()
+	# Load XOP spectra
+	input_folder = project_folder + '/Spectra_Inputs'
+	input_spectra = xop(input_folder + '/xsurface1.dat')
 
-# Create an interpolation object based on angle
-# Passing in an angle in mrad will output an interpolated spectra (w/ XOP as reference) 
-spectra_linfit = interp1d(input_spectra['Angle'], input_spectra['Power'], axis=0)
+	# Find the angles corresponding to the 2018-1 image vertical pixels
+	angles_mrad, flatfield_avg = spectra_angles()
 
-# Create an array containing spectra corresponding to each row of the 2018-1 images
-spectra2D = spectra_linfit(angles_mrad)
+	# Find the index that best approximates middle (angle = 0)
+	middle_index = np.argmin(abs(angles_mrad))
 
-# Grab the edge spectra
-spectra_edge = spectra2D[25]
+	# Create an interpolation object based on angle
+	# Passing in an angle in mrad will output an interpolated spectra (w/ XOP as reference) 
+	spectra_linfit = interp1d(input_spectra['Angle'], input_spectra['Power'], axis=0)
 
-# Load NIST XCOM attenuation curves
-air_atten = xcom(input_folder + '/air.txt', att_column=5)
-Be_atten = xcom(input_folder + '/Be.txt', att_column=5)
-YAG_atten = xcom(input_folder + '/YAG.txt', att_column=3)
-LuAG_atten = xcom(input_folder + '/Al5Lu3O12.txt', att_column=3)
+	# Create an array containing spectra corresponding to each row of the 2018-1 images
+	spectra2D = spectra_linfit(angles_mrad)
 
-# Reshape XCOM x-axis to match XOP
-air_atten = xcom_reshape(air_atten, input_spectra['Energy'])
-Be_atten = xcom_reshape(Be_atten, input_spectra['Energy'])
-YAG_atten = xcom_reshape(YAG_atten, input_spectra['Energy'])
-LuAG_atten = xcom_reshape(LuAG_atten, input_spectra['Energy'])
+	# Middle spectra
+	spectra_middle = spectra2D[middle_index, :]
 
-## EPL in cm
-# Air EPL
-air_epl = 70
+	# Load NIST XCOM attenuation curves
+	YAG_atten = xcom(input_folder + '/YAG.txt', att_column=3)
+	LuAG_atten = xcom(input_folder + '/Al5Lu3O12.txt', att_column=3)
 
-# Be window EPL
-# See Alan's 'Spray Diagnostics at the Advanced Photon Source 7-BM Beamline' (3 Be windows)
-Be_epl = 0.075
+	# Reshape XCOM x-axis to match XOP
+	YAG_atten = xcom_reshape(YAG_atten, input_spectra['Energy'])
+	LuAG_atten = xcom_reshape(LuAG_atten, input_spectra['Energy'])
 
-# Scintillator EPL
-YAG_epl = 0.05      # 500 um
-LuAG_epl = 0.01     # 100 um
+	# Scintillator EPL
+	YAG_epl = 0.05      # 500 um
+	LuAG_epl = 0.01     # 100 um
 
-## Density in g/cm^3
-# Air density
-air_den = 0.001275
+	## Density in g/cm^3
+	# Scintillator densities from Crytur <https://www.crytur.cz/materials/yagce/>
+	YAG_den =  4.57
+	LuAG_den = 6.73
 
-# Beryllium density
-Be_den = 1.85
+	## Apply Beer-Lambert Law
+	# Find detected scintillator spectrum (along middle) and response curves (no filters)
+	LuAG_response = scintillator_response2D(spectra_linfit, LuAG_atten, LuAG_den, LuAG_epl)
+	YAG_response = scintillator_response2D(spectra_linfit, YAG_atten, YAG_den, YAG_epl)
 
-# Scintillator densities from Crytur <https://www.crytur.cz/materials/yagce/>
-YAG_den =  4.57
-LuAG_den = 6.73
+	# Find detected spectra (no filters)
+	LuAG_detected_nofilters = np.array([x*LuAG_response for x in spectra2D])
+	YAG_detected_nofilters = np.array([x*YAG_response for x in spectra2D])
 
-### Apply Beer-Lambert law
-## Scintillator response
-# Use the middle spectrum (highest peak) for the scintillator response curve (assume indep. of spectral intensity)
-spectra_middle = spectra_linfit(0)
+	# Use filters
+	LuAG = map(lambda x: filtered_spectra(input_folder, input_spectra, x, LuAG_response), spectra2D)
+	LuAG = list(LuAG)
+	spectra_filtered = np.swapaxes(LuAG, 0, 1)[0]
+	LuAG_detected = np.swapaxes(LuAG, 0, 1)[1]
 
-# Find the detected spectra through the scintillators (transmitted spectra) at middle
-LuAG_detected = beer_lambert(spectra_middle, LuAG_atten['Attenuation'], LuAG_den, LuAG_epl)
-YAG_detected = beer_lambert(spectra_middle, YAG_atten['Attenuation'], YAG_den, YAG_epl)
+	YAG = map(lambda x: filtered_spectra(input_folder, input_spectra, x, YAG_response), spectra2D)
+	YAG = list(YAG)
+	YAG_detected = np.swapaxes(YAG, 0, 1)[1]
 
-# Same as above but on edges
-LuAG_detected_edge = beer_lambert(spectra_edge, LuAG_atten['Attenuation'], LuAG_den, LuAG_epl)
-YAG_detected_edge = beer_lambert(spectra_edge, YAG_atten['Attenuation'], YAG_den, YAG_epl)
+	## Plot figures
+	plot_folder = project_folder + '/Figures/Energy_Spectra'
+	if not os.path.exists(plot_folder):
+	    os.makedirs(plot_folder)
 
-# Find the scintillator response at middle
-LuAG_response = LuAG_detected / spectra_middle
-YAG_response = YAG_detected / spectra_middle
+	# Create vertical integrated power curves
+	integrated_xray = np.trapz(spectra2D, input_spectra['Energy'], axis=1)
+	integrated_xray_filtered = np.trapz(spectra_filtered, input_spectra['Energy'], axis=1)
+	integrated_LuAG = np.trapz(LuAG_detected_nofilters, input_spectra['Energy'], axis=1)
+	integrated_YAG = np.trapz(YAG_detected_nofilters, input_spectra['Energy'], axis=1)
+	integrated_LuAG_filtered = np.trapz(LuAG_detected, input_spectra['Energy'])
+	integrated_YAG_filtered = np.trapz(YAG_detected, input_spectra['Energy'])
 
-# Same as above but on edges
-LuAG_response_edge = LuAG_detected_edge / spectra_edge
-YAG_response_edge = YAG_detected_edge / spectra_edge
+	# Integrated plots
+	plt.figure()
+	plt.plot(integrated_xray, color='black', linewidth=1.5, label='X-ray Beam')
+	plt.plot(integrated_xray_filtered, color='black', linestyle='--', linewidth=1.5, label='X-ray Beam (filtered)')
+	plt.plot(integrated_LuAG, color='cornflowerblue', linewidth=1.5, label='LuAG')
+	plt.plot(integrated_LuAG_filtered, color='cornflowerblue', linestyle='--', linewidth=1.5, label='LuAG (filtered)')
+	plt.plot(integrated_YAG, color='seagreen', linewidth=1.5, label='YAG')
+	plt.plot(integrated_YAG_filtered, color='seagreen', linestyle='--', linewidth=1.5, label='YAG (filtered)')
+	plt.legend()
+	plt.savefig(plot_folder + '/integrated_power.png')
+	plt.close()
 
-## Filtered spectrum
-# Middle spectrum
-spectra_middle_filtered = beer_lambert(spectra_middle, air_atten['Attenuation'], air_den, air_epl)
-spectra_middle_filtered = beer_lambert(spectra_middle_filtered, Be_atten['Attenuation'], Be_den, Be_epl)
-# Use Ben's correction method
-spectra_middle_filtered = beer_lambert(spectra_middle_filtered, air_atten['Attenuation'], air_den, 50)
+	# Integrated and normalized (filtered) plots
+	plt.figure()
+	plt.plot(integrated_xray_filtered / max(integrated_xray_filtered), zorder=1, color='black', linewidth=1.5, label='X-ray Beam (filtered)')
+	plt.plot(integrated_LuAG_filtered / max(integrated_LuAG_filtered), zorder=2, color='cornflowerblue', linewidth=1.5, label='LuAG (filtered)')
+	plt.plot(integrated_YAG_filtered / max(integrated_YAG_filtered), zorder=3, color='seagreen', linewidth=1.5, label='YAG (filtered)')
+	plt.plot(flatfield_avg / max(flatfield_avg), zorder=4, color='red', linewidth=1.5, label='Flat Field')
+	plt.legend()
+	plt.savefig(plot_folder + '/integrated_norm_power.png')
+	plt.close()
 
-# Edge spectrum
-spectra_edge_filtered = beer_lambert(spectra_edge, air_atten['Attenuation'], air_den, air_epl)
-spectra_edge_filtered = beer_lambert(spectra_edge_filtered, Be_atten['Attenuation'], Be_den, Be_epl)
-# Use Ben's correction method
-spectra_edge_filtered = beer_lambert(spectra_edge_filtered, air_atten['Attenuation'], air_den, 50)
+	# Scintillator response
+	plt.figure()
+	plt.plot(input_spectra['Energy'] / 1000, LuAG_response, linewidth=1.5, color='cornflowerblue', label='LuAG')
+	plt.plot(input_spectra['Energy'] / 1000, YAG_response, linewidth=1.5, color='seagreen', label='YAG')
+	plt.legend()
+	plt.xlabel('Energy (keV)')
+	plt.ylabel('Scintillator Response')
+	plt.savefig(plot_folder + '/scintillator_response.png')
+	plt.close()
 
-## Filtered (detected)
-# Middle spectrum
-LuAG_detected_filtered = spectra_middle_filtered * LuAG_response
-YAG_detected_filtered = spectra_middle_filtered * YAG_response
+	# Spectra along middle
+	plt.figure()
+	plt.plot(input_spectra['Energy'] / 1000, spectra_middle, linewidth=1.5, color='black', label='Incident')
+	plt.plot(input_spectra['Energy'] / 1000, spectra_filtered[middle_index, :], linewidth=1.5, color='black', linestyle='--', label='Incident (filtered)')
+	plt.plot(input_spectra['Energy'] / 1000, LuAG_detected_nofilters[middle_index, :], linewidth=1.5, color='cornflowerblue', label='LuAG')
+	plt.plot(input_spectra['Energy'] / 1000, LuAG_detected[middle_index, :], linewidth=1.5, color='cornflowerblue', linestyle='--', label='LuAG (filtered)')
+	plt.plot(input_spectra['Energy'] / 1000, YAG_detected_nofilters[middle_index, :], linewidth=1.5, color='seagreen', label='YAG')
+	plt.plot(input_spectra['Energy'] / 1000, YAG_detected[middle_index, :], linewidth=1.5, color='seagreen', linestyle='--', label='YAG (filtered)')
+	plt.legend()
+	plt.xlabel('Energy (keV)')
+	plt.ylabel('Spectral Power (mW/keV)')
+	plt.savefig(plot_folder + '/middle_spectra.png')
+	plt.close()
 
-# Edge spectrum
-LuAG_detected_edge_filtered = spectra_edge_filtered * LuAG_response
-YAG_detected_edge_filtered = spectra_edge_filtered * YAG_response
+	# LuAG filtered spectra at various locations
+	plt.figure()
+	plt.plot(input_spectra['Energy'] / 1000, LuAG_detected[middle_index, :], linewidth=1.5, color='black', label='{0}'.format(middle_index))
+	plt.plot(input_spectra['Energy'] / 1000, LuAG_detected[middle_index+50, :], linewidth=1.5, color='blue', label='{0}'.format(middle_index+50))
+	plt.plot(input_spectra['Energy'] / 1000, LuAG_detected[middle_index-50, :], linewidth=1.5, color='blue', linestyle='--', label='{0}'.format(middle_index-50))
+	plt.plot(input_spectra['Energy'] / 1000, LuAG_detected[middle_index+100, :], linewidth=1.5, color='red', label='{0}'.format(middle_index+100))
+	plt.plot(input_spectra['Energy'] / 1000, LuAG_detected[middle_index-100, :], linewidth=1.5, color='red', linestyle='--', label='{0}'.format(middle_index-100))
+	plt.legend()
+	plt.title('Filtered')
+	plt.xlabel('Energy (keV)')
+	plt.ylabel('Spectral Power (mW/keV)')
+	plt.savefig(plot_folder + '/LuAG_filtered_spectra.png')
+	plt.close()
 
-## Plot figures
-plot_folder = project_folder + '/Figures/Energy_Spectra'
-if not os.path.exists(plot_folder):
-    os.makedirs(plot_folder)
+	# YAG filtered spectra at various locations
+	plt.figure()
+	plt.plot(input_spectra['Energy'] / 1000, YAG_detected[middle_index, :], linewidth=1.5, color='black', label='{0}'.format(middle_index))
+	plt.plot(input_spectra['Energy'] / 1000, YAG_detected[middle_index+50, :], linewidth=1.5, color='blue', label='{0}'.format(middle_index+50))
+	plt.plot(input_spectra['Energy'] / 1000, YAG_detected[middle_index-50, :], linewidth=1.5, color='blue', linestyle='--', label='{0}'.format(middle_index-50))
+	plt.plot(input_spectra['Energy'] / 1000, YAG_detected[middle_index+100, :], linewidth=1.5, color='red', label='{0}'.format(middle_index+100))
+	plt.plot(input_spectra['Energy'] / 1000, YAG_detected[middle_index-100, :], linewidth=1.5, color='red', linestyle='--', label='{0}'.format(middle_index-100))
+	plt.legend()
+	plt.title('Filtered')
+	plt.xlabel('Energy (keV)')
+	plt.ylabel('Spectral Power (mW/keV)')
+	plt.savefig(plot_folder + '/YAG_filtered_spectra.png')
+	plt.close()
 
-# Scintillator response
-plt.figure()
-plt.plot(input_spectra['Energy'] / 1000, LuAG_response, linewidth=1.5, color='cornflowerblue', label='LuAG')
-plt.plot(input_spectra['Energy'] / 1000, LuAG_response_edge, linestyle='--', linewidth=2.5, color='cornflowerblue', label='LuAG (edge)')
-plt.plot(input_spectra['Energy'] / 1000, YAG_response, linewidth=1.5, color='seagreen', label='YAG')
-plt.plot(input_spectra['Energy'] / 1000, YAG_response_edge, linestyle='--', linewidth=2.5, color='seagreen', label='YAG (edge)')
-plt.legend()
-plt.xlabel('Energy (keV)')
-plt.ylabel('Scintillator Response')
-plt.savefig(plot_folder + '/scintillator_response.png')
+	# X-ray spectra at various locations
+	plt.figure()
+	plt.plot(input_spectra['Energy'] / 1000, spectra2D[middle_index, :], linewidth=1.5, color='black', label='{0}'.format(middle_index))
+	plt.plot(input_spectra['Energy'] / 1000, spectra2D[middle_index+50, :], linewidth=1.5, color='blue', label='{0}'.format(middle_index+50))
+	plt.plot(input_spectra['Energy'] / 1000, spectra2D[middle_index-50, :], linewidth=1.5, color='blue', linestyle='--', label='{0}'.format(middle_index-50))
+	plt.plot(input_spectra['Energy'] / 1000, spectra2D[middle_index+100, :], linewidth=1.5, color='red', label='{0}'.format(middle_index+100))
+	plt.plot(input_spectra['Energy'] / 1000, spectra2D[middle_index-100, :], linewidth=1.5, color='red', linestyle='--', label='{0}'.format(middle_index-100))
+	plt.legend()
+	plt.title('Unfiltered')
+	plt.xlabel('Energy (keV)')
+	plt.ylabel('Spectral Power (mW/keV)')
+	plt.savefig(plot_folder + '/xray_spectra.png')
+	plt.close()
 
-# Spectra along middle
-plt.figure()
-plt.plot(input_spectra['Energy'] / 1000, 1000*spectra_middle / (input_spectra['Energy'] / 1000), linewidth=1.5, color='black', label='Incident')
-plt.plot(input_spectra['Energy'] / 1000, 1000*spectra_middle_filtered / (input_spectra['Energy'] / 1000), linewidth=1.5, color='red', label='Filtered')
-plt.plot(input_spectra['Energy'] / 1000, 1000*LuAG_detected_filtered / (input_spectra['Energy'] / 1000), linewidth=1.5, color='cornflowerblue', linestyle='--', label='LuAG (detected)')
-plt.plot(input_spectra['Energy'] / 1000, 1000*YAG_detected_filtered / (input_spectra['Energy'] / 1000), linewidth=1.5, color='seagreen', linestyle='--', label='YAG (detected)')
-plt.legend()
-plt.xlabel('Energy (keV)')
-plt.ylabel('Spectral Power (mW/keV)')
-plt.savefig(plot_folder + '/middle_spectra.png')
 
-# Spectra along middle
-plt.figure()
-plt.plot(input_spectra['Energy'] / 1000, 1000*spectra_edge / (input_spectra['Energy'] / 1000), linewidth=1.5, color='black', label='Incident')
-plt.plot(input_spectra['Energy'] / 1000, 1000*spectra_edge_filtered / (input_spectra['Energy'] / 1000), linewidth=1.5, color='red', label='Filtered')
-plt.plot(input_spectra['Energy'] / 1000, 1000*LuAG_detected_edge_filtered / (input_spectra['Energy'] / 1000), linewidth=1.5, color='cornflowerblue', linestyle='--', label='LuAG (detected)')
-plt.plot(input_spectra['Energy'] / 1000, 1000*YAG_detected_edge_filtered / (input_spectra['Energy'] / 1000), linewidth=1.5, color='seagreen', linestyle='--', label='YAG (detected)')
-plt.legend()
-plt.xlabel('Energy (keV)')
-plt.ylabel('Spectral Power (mW/keV)')
-plt.savefig(plot_folder + '/edge_spectra.png')
+# Process this script
+if __name__ == '__main__':
+	main()
