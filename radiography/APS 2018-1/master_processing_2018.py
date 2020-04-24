e9db8e75d1a601056e6ae2e6899cbd937ce74648
narahma2 <rahmann@purdue.edu>
2020-04-24 16:48:38 -0400

    UPDATE: Add energy spectra plots

--- radiography/APS 2018-1/master_processing_2018.py
+++ radiography/APS 2018-1/master_processing_2018.py
@@ -8,10 +8,14 @@ Runs all the major scripts for full processing of the APS 2018-1 data set.
 """
 
 from whitebeam_2018 import main as whitebeam_2018
+from energy_spectra_2018_plots import main as energy_spectra_2018_plots
 from jet_processing import main as jet_processing
 
 # Build the whitebeam models
 whitebeam_2018()
+
+# Create the energy spectra plots
+energy_spectra_2018_plots()
 
 # Process the uniform jets
 jet_processing()
