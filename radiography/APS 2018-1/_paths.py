b36a33bf0813d9771ce08e03cb87d49f25c4a87a
rahmann@purdue.edu <rahmann@purdue.edu>
2020-05-13 21:28:59 -0400

    Initial commit
    File that sets up the sys path for this project.

---
+++ radiography/APS 2018-1/_paths.py
@@ -0,0 +1,11 @@
+import sys
+
+
+if sys.platform == 'win32':
+    sys.path.append('E:/GitHub/xray/general')
+    sys.path.append('E:/GitHub/xray/temperature')
+    sys_folder = 'R:'
+elif sys.platform == 'linux':
+    sys.path.append('/mnt/e/GitHub/xray/general')
+    sys.path.append('/mnt/e/GitHub/xray/temperature')
+    sys_folder = '/mnt/r/'
