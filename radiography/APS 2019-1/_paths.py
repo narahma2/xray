f2c1593672036456e85ec3a41c9a963fe2889165
rahmann@purdue.edu <rahmann@purdue.edu>
2020-05-14 18:57:26 -0400

    REMOVE: No longer needed

--- radiography/APS 2019-1/_paths.py
+++ radiography/APS 2019-1/_paths.py
@@ -1,11 +1,14 @@
 import sys
 
 
-if sys.platform == 'win32':
-    sys.path.append('E:/GitHub/xray/general')
-    sys.path.append('E:/GitHub/xray/temperature')
-    sys_folder = 'R:'
-elif sys.platform == 'linux':
-    sys.path.append('/mnt/e/GitHub/xray/general')
-    sys.path.append('/mnt/e/GitHub/xray/temperature')
-    sys_folder = '/mnt/r/'
+def paths():
+    if sys.platform == 'win32':
+        sys.path.append('E:/GitHub/xray/general')
+        sys.path.append('E:/GitHub/xray/temperature')
+        sys_folder = 'R:'
+    elif sys.platform == 'linux':
+        sys.path.append('/mnt/e/GitHub/xray/general')
+        sys.path.append('/mnt/e/GitHub/xray/temperature')
+        sys_folder = '/mnt/r/'
+
+    return sys_folder
