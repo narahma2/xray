5b24fd5c6000fdda71e3a528f9caab284eda1f2b
rahmann@purdue.edu <rahmann@purdue.edu>
2020-05-22 12:31:38 -0400

    UPDATE: Modified multi_angle to include flux
    Code also reconfigured to match PEP8 standards.

--- general/Spectra/spectrum_modeling.py
+++ general/Spectra/spectrum_modeling.py
@@ -11,109 +11,132 @@ Returns either  a) multiple spectra for white-beam (vertical variation) setup
 @Last Modified time: 2020-04-03 15:40:47
 """
 
-import pandas as pd
 import numpy as np
+import pandas as pd
 
 
 def multi_angle(xop_path):
     """Used for modeling vertical variation in synchrotron spectra"""
-    # [Angle (mrad), Energy (eV), Spectrosc. Power (Watts/eV/mrad)]
-    spectrum = pd.read_csv(xop_path, sep='\s+', header=None)
-    spectrum.columns = ["Angle", "Energy", "Power"]
+    # [Angle (mrad), Energy (eV), Power (Watts/eV/mrad) or Flux
+    #   (Photons/s/0.1%bw/mrad)]
+    spct = pd.read_csv(xop_path, sep='\s+', header=None)
+    spct.columns = ['Angle', 'Energy', 'Intensity']
 
-    # Can sort the spectrum DataFrame if needed, however will be split into lists instead
-    spectrum = spectrum.sort_values(["Angle", "Energy"])
-    spectrum = spectrum.reset_index(drop=True)
-    
-    # spectrum["Power"] = spectrum["Power"] * spectrum["Energy"]
+    # Can sort DataFrame if needed, however will be split into lists instead
+    spct = spct.sort_values(['Angle', 'Energy'])
+    spct = spct.reset_index(drop=True)
 
-    # Convert to mW/keV
-    spectrum["Power"] *= 1000000
+    # Scale intensity to visible photon count
+    # Logic being that 10 keV x-ray photons emit 10x more visible photons 
+    #   than 1 keV x-ray photons
+    spct['Intensity'] = spct['Intensity'] * spct['Energy']
 
-    # List of spectrum separated by angle (e.g. if there are 336 unique angles, this returns len(spectrum_list) = 336
-    spectrum_list = [v for k, v in spectrum.groupby("Angle")]
+    # List of spectrum separated by angle 
+    # e.g. if there are 336 unique angles, this has len(spct_list) = 336
+    spct_list = [v for k, v in spct.groupby('Angle')]
 
-    xop_spectrum = {"Angle": [np.mean(item["Angle"]) for item in spectrum_list], "Energy": spectrum_list[0]["Energy"], "Power": [item["Power"] for item in spectrum_list]}
+    xop_spct = {
+                'Angle': [np.mean(item['Angle']) for item in spct_list],
+                'Energy': spct_list[0]['Energy'],
+                'Intensity': [item['Intensity'] for item in spct_list]
+                }
 
-    return xop_spectrum
+    return xop_spct
+
 
 def tube_xop(xop_path):
     """Used for modeling the tube source spectra"""
     # [Energy (eV), Flux (photons/1keV(bw)/mA/mm^2(@1m)/sec)]
-    spectrum = pd.read_csv(xop_path, sep='\s+', names=["Energy", "Flux"], skiprows=4)
-    
+    spctm = pd.read_csv(
+                        xop_path,
+                        sep='\s+',
+                        names=['Energy', 'Flux'],
+                        skiprows=4
+                        )
+
     # Convert Flux to units of (photons/mA/mm^2(@1m)/sec)
-    spectrum["Flux"] = spectrum["Flux"] * (spectrum["Energy"] / 1000)
-    
+    spctm['Flux'] = spctm['Flux'] * (spctm['Energy'] / 1000)
+
     # Convert Flux to Power in units of (W/mA/mm^2(@1m))
-    spectrum["Flux"] = spectrum["Flux"] * (spectrum["Energy"] * (1.602E-19))    # Convert eV to Joules, Watts = J/s
-    spectrum.columns = ["Energy", "Power"]
-    
-    return spectrum
+    # Convert eV to Joules, Watts = J/s
+    spctm['Flux'] = spctm['Flux'] * (spctm['Energy'] * (1.602E-19))
+    spctm.columns = ['Energy', 'Power']
+
+    return spctm
+
 
 def xcom(xcom_path, att_column=3):
-    xcom_spectra = pd.read_csv(xcom_path, sep='\t')
+    xcom_spct = pd.read_csv(xcom_path, sep='\t')
 
     # Convert abscissa from MeV to eV
-    xcom_spectra.iloc[:, 0] = xcom_spectra.iloc[:, 0] * 1E6
+    xcom_spct.iloc[:, 0] = xcom_spct.iloc[:, 0] * 1E6
 
     # Add residual value to edge locations for interpolation
-    for i in range(len(xcom_spectra.iloc[:, 0]) - 1):
-        if xcom_spectra.iloc[i, 0] == xcom_spectra.iloc[i + 1, 0]:
-            xcom_spectra.iloc[i, 0] -= 1E-6
-            xcom_spectra.iloc[i + 1, 0] += 1E-6
+    for i in range(len(xcom_spct.iloc[:, 0]) - 1):
+        if xcom_spct.iloc[i, 0] == xcom_spct.iloc[i + 1, 0]:
+            xcom_spct.iloc[i, 0] -= 1E-6
+            xcom_spct.iloc[i + 1, 0] += 1E-6
 
     # Rename column name
-    xcom_spectra = xcom_spectra.rename(columns={'Photon Energy (MeV)': 'Photon Energy (eV)'})
+    xcom_spct = xcom_spct.rename(columns={'Photon Energy (MeV)':
+                                          'Photon Energy (eV)'})
 
     # Energy is in eV, Attenuation is in cm^2/g
-    xcom_spectra = {"Energy": xcom_spectra.iloc[:, 0], "Attenuation": xcom_spectra.iloc[:, att_column]}
+    xcom_spct = {
+                 'Energy': xcom_spct.iloc[:, 0],
+                 'Attenuation': xcom_spct.iloc[:, att_column]
+                 }
 
-    return xcom_spectra
+    return xcom_spct
 
 
-def xcom_reshape(xcom_spectra, xop_abscissa):
-    """Linear interpolation to reconfigure the x-axis of the xcom spectra to match XOP input"""
-    y = np.interp(xop_abscissa, xcom_spectra["Energy"], xcom_spectra["Attenuation"])
-    y = {"Energy": xop_abscissa, "Attenuation": y}
+def xcom_reshape(xcom_spct, xop_abscissa):
+    """Linear interp. to reconfigure the x-axis of the xcom spectra to XOP"""
+    y = np.interp(
+                  xop_abscissa,
+                  xcom_spct['Energy'],
+                  xcom_spct['Attenuation']
+                  )
+
+    y = {'Energy': xop_abscissa, 'Attenuation': y}
 
     return y
 
 
 def beer_lambert(incident, attenuation, density, epl):
-    """Beer-Lambert Law with attenuation coefficient on a mass basis, outputs EPL in [length]"""
-    transmitted = incident * np.exp(-attenuation * density * epl)
+    """Beer-Lambert with atten. coeff. on mass basis, EPL in [length]"""
+    T = incident * np.exp(-attenuation * density * epl)
 
-    return transmitted
+    return T
 
 
 def visible_light(incident, scint_trans):
-    """Converts scintillator transmission to scintillator absorption (image on camera)"""
+    """Converts scint. transmission to scint. absorption image on camera)"""
     vl = [y1 - y2 for (y1,y2) in zip(incident, scint_trans)]
-    
+
     return vl
 
 
 def beer_lambert_unknown(incident, attenuation, density, epl):
-    transmitted = [incident * np.exp(-attenuation * density * length) for length in epl]
-    
-    return transmitted
+    T = [incident * np.exp(-attenuation * density * length) for length in epl]
+
+    return T
 
 
 def density_KIinH2O(concentration):
     """Returns the density of known KI concentration in g/cm^3"""
     conc = [0, 4, 20, 26, 30, 50]
     dens = [1, 1.028, 1.168, 1.227, 1.26, 1.54]
-    
+
     p1 = 0.078E-5
     p2 = 0.006199
     p3 = 1.002
     T = 50
     f = p1*T**2 + p2*T + p3
-    
+
     # Density of 10% KI in water by mass
     # rho_10_KI = p1*10^2 + p2*10 + p3
-    
+
     rho = p1*(concentration)**2 + p2*(concentration) + p3
-    
+
     return rho
