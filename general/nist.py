3e2f3d4ef55430275450fcc309d2fe7842d79199
rahmann@purdue.edu <rahmann@purdue.edu>
2020-06-22 17:44:06 -0400

    UPDATE: Combined attenuation coefficient functions into one

--- general/nist.py
+++ general/nist.py
@@ -2,40 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-def xcom(xcom_path, att_column=3):
-    """
-    Takes in the older XCOM .txt file and returns the desired attenuation
-    coefficient.
-    =============
-    --VARIABLES--
-    xcom_path:      Path to .txt file.
-    att_column:     Desired attenuation coefficient.
-    """
-    xcom_spct = pd.read_csv(xcom_path, sep='\t')
-
-    # Convert abscissa from MeV to eV
-    xcom_spct.iloc[:, 0] = xcom_spct.iloc[:, 0] * 1E6
-
-    # Add residual value to edge locations for interpolation
-    for i in range(len(xcom_spct.iloc[:, 0]) - 1):
-        if xcom_spct.iloc[i, 0] == xcom_spct.iloc[i + 1, 0]:
-            xcom_spct.iloc[i, 0] -= 1E-6
-            xcom_spct.iloc[i + 1, 0] += 1E-6
-
-    # Rename column name
-    xcom_spct = xcom_spct.rename(columns={'Photon Energy (MeV)':
-                                          'Photon Energy (eV)'})
-
-    # Energy is in eV, Attenuation is in cm^2/g
-    xcom_spct = {
-                 'Energy': xcom_spct.iloc[:, 0],
-                 'Attenuation': xcom_spct.iloc[:, att_column]
-                 }
-
-    return xcom_spct
-
-
-def mass_atten(molec, comp=[1]):
+def mass_atten(molec, comp=[1], xcom=1, col=None):
     """
     Retrieves the mass attenuation coefficient data from NIST.
     See: <https://www.nist.gov/pml/x-ray-mass-attenuation-coefficients>
@@ -48,8 +15,30 @@ def mass_atten(molec, comp=[1]):
                 10% KI in H2O: ['H2O', 'KI']
     comp:       Mass composition of the mixture (optional). Order matches the
                 molec variable, e.g. for 10% KI in H2O: [0.9, 0.1].
+    xcom:       1: first database (w/ specific interactions listed) (default)
+                2: new updated energy-absorption coefficient (total coeff).
+    col:        Which attenuation coefficient to return, depedent on xcom.
+                if xcom == 1, col corresponds to:
+                    1: Coherent scattering
+                    2: Incoherent scattering
+                    3: Photoelectric absorption
+                    4: Pair production in nuclear field
+                    5: Pair production in electron field
+                    6: Total attenuation with coherent scattering
+                    7: Total attenuation without coherent scattering (default)
+                if xcom == 2, col corresponds to:
+                    1: Total mass atten. coeff.
+                    2: Energy-absorption corrected atten. coeff. (default)
     """
-    import re
+    # Select desired database and set default column
+    if xcom == 1:
+        database = '/resources/nist_xcom1.xlsx'
+        if col is None:
+            col = 7
+    elif xcom == 2:
+        database = '/resources/nist_xcom2.xlsx'
+        if col is None:
+            col = 2
 
     # Convert to arrays
     molec = np.array(molec)
@@ -66,8 +55,7 @@ def mass_atten(molec, comp=[1]):
             return
 
     # Create list that contain the data for each material
-    molec_mu = len(molec) * [None]
-    molec_mu_en = len(molec) * [None]
+    molec_coeff = len(molec) * [None]
     molec_energy = len(molec) * [None]
 
     for i, x in enumerate(molec):
@@ -76,15 +64,13 @@ def mass_atten(molec, comp=[1]):
 
         atom_data = len(atoms) * [None]
         interp_mu = len(atoms) * [None]
-        interp_mu_en = len(atoms) * [None]
-        raw_mu = len(atoms) * [None]
-        raw_mu_en = len(atoms) * [None]
+        raw_coeff = len(atoms) * [None]
         raw_energy = len(atoms) * [None]
 
         # Read in data for each atom
         for j, y in enumerate(atoms):
             atom_data[j] = pd.read_excel(
-                                         'nist_mass_atten_coeff.xlsx',
+                                         database,
                                          sheet_name=y,
                                          skiprows=2
                                          )
@@ -93,53 +79,33 @@ def mass_atten(molec, comp=[1]):
             raw_energy[j] = atom_data[j].Energy.to_numpy() * 1000
 
             # Get coefficient values
-            raw_mu[j] = atom_data[j].mu
-            raw_mu_en[j] = atom_data[j].mu_en
+            raw_coeff[j] = atom_data[j].iloc[:, col]
 
         # Combine coefficients into a common energy axis
-        [molec_energy[i], atom_mu] = common_energy(raw_mu, raw_energy)
-        [_, atom_mu_en] = common_energy(raw_mu_en, raw_energy)
-
+        [molec_energy[i], atom_coeff] = common_energy(raw_coeff, raw_energy)
 
         # Calculate combined molecule coefficients
-        molec_mu[i] = np.array(
+        molec_coeff[i] = np.array(
                                [
-                                atom_frac[j] * atom_mu[j]
+                                atom_frac[j] * atom_coeff[j]
                                 for j,_ in enumerate(atoms)
                                 ]
                                )
-        molec_mu[i] = molec_mu[i].sum(axis=0)
-
-        molec_mu_en[i] = np.array(
-                                  [
-                                   atom_frac[j] * atom_mu_en[j]
-                                   for j,_ in enumerate(atoms)
-                                   ]
-                                  )
-        molec_mu_en[i] = molec_mu_en[i].sum(axis=0)
+        molec_coeff[i] = molec_coeff[i].sum(axis=0)
 
     # Get common energy axis for all the molecules
-    [energy, mu] = common_energy(molec_mu, molec_energy)
-    [_, mu_en] = common_energy(molec_mu_en, molec_energy)
+    [energy, coeff] = common_energy(molec_coeff, molec_energy)
 
     # Calculate combined mixture coefficients
-    mu = np.array(
-                  [
-                   comp[j] * mu[j]
-                   for j,_ in enumerate(molec)
-                   ]
-                  )
-    mu = mu.sum(axis=0)
-
-    mu_en = np.array(
+    coeff = np.array(
                      [
-                      comp[j] * mu_en[j]
+                      comp[j] * coeff[j]
                       for j,_ in enumerate(molec)
                       ]
                      )
-    mu_en = mu_en.sum(axis=0)
+    coeff = coeff.sum(axis=0)
 
-    return energy, mu, mu_en
+    return {'Energy': energy, 'Attenuation': coeff}
 
 
 def common_energy(coeffs, energies):
@@ -194,6 +160,7 @@ def molecule_info(molec):
     molec:          String containing chemical formula. Must be its simplest
                     form without parentheses, i.e. C8H18 not CH3(CH2)6CH3.
     """
+    import re
     import xlrd
 
     # Get molecule formula
@@ -207,10 +174,11 @@ def molecule_info(molec):
     atom_mass = np.zeros(len(atoms),)
 
     for i, atom in enumerate(atoms):
-        atom_mass[i] = xlrd.open_workbook('nist_mass_atten_coeff.xlsx') \
+        atom_mass[i] = xlrd.open_workbook('/resources/nist_xcom1.xlsx') \
                            .sheet_by_name(atom) \
                            .cell(0, 1) \
                            .value
+        atom_mass[i] *= atom_count[i]
 
     # Get total molecule mass
     molecule_mass = atom_mass.sum()
@@ -218,37 +186,29 @@ def molecule_info(molec):
     # Calculate mass fraction for each atom
     atom_frac = atom_mass / molecule_mass
 
-    return atom_frac
+    return atoms, atom_count, atom_frac
 
 
 def test_plots():
     import matplotlib.pyplot as plt
 
-    h2o = mass_atten(['H2O'])
-    ki = mass_atten(['KI'])
-    ki50 = mass_atten(['H2O', 'KI'], [0.5, 0.5])
+    ki50_xcom1 = mass_atten(['H2O', 'KI'], [0.5, 0.5], xcom=1, col=7)
+    ki50_xcom2 = mass_atten(['H2O', 'KI'], [0.5, 0.5], xcom=2, col=2)
 
     plt.figure()
     plt.plot(
-             h2o[0],
-             h2o[1],
+             ki50_xcom1['Energy'],
+             ki50_xcom1['Attenuation'],
              color='b',
              linestyle='solid',
-             label='H$_2$O'
+             linewidth=3.0,
+             label='50% KI/H$_2$O'
              )
     plt.plot(
-             ki[0],
-             ki[1],
-             color='r',
+             ki50_xcom2['Energy'],
+             ki50_xcom2['Attenuation'],
+             color='b',
              linestyle='solid',
-             linewidth=2.0,
-             label='KI'
-             )
-    plt.plot(
-             ki50[0],
-             ki50[1],
-             color='k',
-             linestyle='dashed',
              linewidth=2.0,
              label='50% KI/H$_2$O'
              )
@@ -259,39 +219,6 @@ def test_plots():
     plt.ylabel('Attenuation Coefficient (cm$^2$/g)')
     plt.title('$\mu$')
     plt.savefig('mu.png')
-    plt.close()
-
-    plt.figure()
-    plt.plot(
-             h2o[0],
-             h2o[2],
-             color='b',
-             linestyle='solid',
-             label='H$_2$O'
-             )
-    plt.plot(
-             ki[0],
-             ki[2],
-             color='r',
-             linestyle='solid',
-             linewidth=2.0,
-             label='KI'
-             )
-    plt.plot(
-             ki50[0],
-             ki50[2],
-             color='k',
-             linestyle='dashed',
-             linewidth=2.0,
-             label='50% KI/H$_2$O'
-             )
-    plt.xlim([0, 200])
-    plt.yscale('log')
-    plt.legend()
-    plt.xlabel('Photon Energy (keV)')
-    plt.ylabel('Attenuation Coefficient (cm$^2$/g)')
-    plt.title('$\mu_{en}$')
-    plt.savefig('mu_en.png')
     plt.close()
 
     return
