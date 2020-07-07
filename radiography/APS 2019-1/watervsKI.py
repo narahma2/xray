05187bff7eb01db93d60335770b890d0f257e1ea
rahmann@purdue.edu <rahmann@purdue.edu>
2020-07-06 23:02:14 -0400

    UPDATE: Figure locations and scales

--- radiography/APS 2019-1/watervsKI.py
+++ radiography/APS 2019-1/watervsKI.py
@@ -11,9 +11,11 @@ import pickle
 import glob
 import numpy as np
 import pandas as pd
+
 import matplotlib.pyplot as plt
 from PIL import Image
 from scipy.signal import savgol_filter, find_peaks, peak_widths
+from general.spectrum_modeling import density_KIinH2O
 from general.misc import create_folder
 
 
@@ -24,9 +26,13 @@ def main():
     spray_conditions = ['water', 'KI']
     spray_indices = [25, 35]
     save_fld = create_folder('{0}/Water_vs_KI/'.format(prj_fld))
+    hdf5_fld = '{0}/HDF5'.format(prj_fld)
 
     water_name = test_matrix['Test'][spray_indices[0]]
     ki_name = test_matrix['Test'][spray_indices[1]]
+
+    water_test = water_name.rsplit('_')[0]
+    ki_test = ki_name.rsplit('_')[0]
 
     cm_px = 0.05 / 52   # See 'Pixel Size' in Excel workbook
 
@@ -55,6 +61,31 @@ def main():
 
     ki_mdpt = int(round(np.mean(spray_middle[35:])))
 
+    # Load HDF5 monobeam scan for J ~ 3.26
+    scans = [5124, 5125, 5126]
+    yp = np.array([1.15, 2.3, 4.6])
+    wbp = yp / (cm_px * 10)
+
+    EPL = len(scans) * [None]
+    x = len(scans) * [None]
+
+    for n, scan in enumerate(scans):
+        f = h5py.File('{0}/Scan_{1}.hdf5'.format(hdf5_fld, scan), 'r')
+        x[n] = np.array(f['X'])
+        BIM = np.array(f['BIM'])
+        PIN = np.array(f['PINDiode'])
+        extinction_length = np.log(BIM / PIN)
+        offset = np.median(extinction_length[0:10])
+        extinction_length -= offset
+
+        # Attenuation coefficient (total w/o coh. scattering - cm^2/g)
+        # Convert to mm^2/g for mm, multiply by density in g/mm^3
+        # Pure water @ 8 keV
+        # <https://physics.nist.gov/PhysRefData/Xcom/html/xcom1.html>
+        atten_coeff = (1.006*10*(10*10))*(0.001)
+
+        EPL[n] = extinction_length / atten_coeff
+
     #%% Plots
     xx = np.linspace(1,768,768)
     xx = xx * cm_px * 10       # in mm
@@ -64,108 +95,132 @@ def main():
     yy = yy * cm_px * 10       # in mm
 
     # Time averaged plots
-    # FIG1: Horizontal Plot @ y/d = 0.75
+    # FIG1: Horizontal Plot @ y = 1.15 mm
     plt.figure()
     plt.plot(
              xx[0::5],
-             10000*water[188][0::5],
+             10000*water[125][0::5],
              color='b',
              marker='o',
              fillstyle='none',
-             label='Water Averaged'
+             label='Water WB {0}'.format(water_test)
              )
     plt.plot(
              xx[0::5],
-             10000*ki[188][0::5],
+             10000*ki[125][0::5],
              color='g',
              marker='^',
              fillstyle='none',
-             label='KI Averaged'
+             label='KI WB {0}'.format(ki_test)
              )
-    plt.title('Time Averaged Comparison at y/d = 0.75')
+    plt.plot(
+             x[0],
+             1000*np.mean(EPL[0], axis=1),
+             color='r',
+             marker='s',
+             fillstyle='none',
+             label='Water MB {0}'.format(scans[0])
+             )
+    plt.title('Time Averaged Comparison at y = 1.15 mm')
     plt.xlim([-3, 3])
-    plt.ylim([-200, 2500])
+    plt.ylim([-200, 3500])
     plt.xlabel('Horizontal Location (mm)')
     plt.ylabel('Equivalent Path Length ($\mu$m)')
     plt.legend()
-    plt.savefig(save_fld + 'timeaveraged_yd075.png')
+    plt.savefig(save_fld + 'timeaveraged_y115.png')
     plt.close()
 
-    # FIG2: Horizontal Plot @ y/d = 1.25
+    # FIG2: Horizontal Plot @ y = 2.30
     plt.figure()
     plt.plot(
              xx[0::5],
-             10000*water[314][0::5],
+             10000*water[251][0::5],
              color='b',
              marker='o',
              fillstyle='none',
-             label='Water Averaged'
+             label='Water WB {0}'.format(water_test)
              )
     plt.plot(
              xx[0::5],
-             10000*ki[314][0::5],
+             10000*ki[251][0::5],
              color='g',
              marker='^',
              fillstyle='none',
-             label='KI Averaged'
+             label='KI WB {0}'.format(ki_test)
              )
-    plt.title('Time Averaged Comparison at y/d = 1.25')
+    plt.plot(
+             x[1],
+             1000*np.mean(EPL[1], axis=1),
+             color='r',
+             marker='s',
+             fillstyle='none',
+             label='Water MB {0}'.format(scans[1])
+             )
+    plt.title('Time Averaged Comparison at y = 2.30')
     plt.xlim([-3, 3])
-    plt.ylim([-200, 2500])
+    plt.ylim([-200, 3500])
     plt.xlabel('Horizontal Location (mm)')
     plt.ylabel('Equivalent Path Length ($\mu$m)')
     plt.legend()
-    plt.savefig(save_fld + 'timeaveraged_yd125.png')
+    plt.savefig(save_fld + 'timeaveraged_y230.png')
     plt.close()
 
-    # FIG3: Horizontal Plot @ y/d = 1.75
+    # FIG3: Horizontal Plot @ y = 4.60
     plt.figure()
     plt.plot(
              xx[0::5],
-             10000*water[439][0::5],
+             10000*water[502][0::5],
              color='b',
              marker='o',
              fillstyle='none',
-             label='Water Averaged'
+             label='Water WB {0}'.format(water_test)
              )
     plt.plot(
              xx[0::5],
-             10000*ki[439][0::5],
+             10000*ki[502][0::5],
              color='g',
              marker='^',
              fillstyle='none',
-             label='KI Averaged'
+             label='KI WB {0}'.format(ki_test)
              )
-    plt.title('Time Averaged Comparison at y/d = 1.75')
+    plt.plot(
+             x[0],
+             1000*np.mean(EPL[2], axis=1),
+             color='r',
+             marker='s',
+             fillstyle='none',
+             label='Water MB {0}'.format(scans[2])
+             )
+    plt.title('Time Averaged Comparison at y = 4.60')
     plt.xlim([-3, 3])
-    plt.ylim([-200, 2500])
+    plt.ylim([-200, 3500])
     plt.xlabel('Horizontal Location (mm)')
     plt.ylabel('Equivalent Path Length ($\mu$m)')
     plt.legend()
-    plt.savefig(save_fld + 'timeaveraged_yd175.png')
+    plt.savefig(save_fld + 'timeaveraged_y460.png')
     plt.close()
 
-    # FIG4: Plot down the centerline 
+    # FIG4: Plot down the centerline
     plt.figure()
     plt.plot(
              yy[54:-30][0::5],
-             10000*water[54:-30,water_mdpt][0::5],
+             10000*water[54:-30, water_mdpt][0::5],
              color='b',
              marker='o',
              fillstyle='none',
-             label='Water Averaged'
+             label='Water {0} @ {1}'.format(water_test, water_mdpt)
              )
     plt.plot(
              yy[54:-30][0::5],
-             10000*ki[54:-30,ki_mdpt][0::5],
+             10000*ki[54:-30, ki_mdpt][0::5],
              color='g',
              marker='^',
              fillstyle='none',
-             label='KI Averaged'
+             label='KI {0} @ {1}'.format(ki_test, ki_mdpt)
              )
     plt.title('Time Averaged Comparison Down Centerline')
     plt.xlim([yy[50], yy[-25]])
-    plt.ylim([1000, 3500])
+    plt.ylim([-200, 3500])
     plt.xlabel('Vertical Location (mm)')
     plt.ylabel('Equivalent Path Length ($\mu$m)')
     plt.legend()
