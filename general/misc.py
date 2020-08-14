211775735309553cffd6881fb8c28f020c24fb98
rahmann@purdue.edu <rahmann@purdue.edu>
2020-08-14 05:12:43 -0900

    Added exportEmf()

--- general/misc.py
+++ general/misc.py
@@ -1,4 +1,6 @@
 import os
+import subprocess
+import matplotlib.pyplot as plt
 
 
 def create_folder(new_folder):
@@ -6,3 +8,30 @@ def create_folder(new_folder):
                 os.makedirs(new_folder)
 
         return new_folder
+
+
+def exportEmf(savePath, plotName, fig=None, keepSVG=False):
+    """
+    Save a figure as an emf file.
+
+    Parameters
+    ----------
+    savePath:   str, the path to the directory you want the image saved in
+    plotName:   str, the name of the image
+    fig:        matplotlib figure, (optional, default uses gca)
+    keepSVG:    bool, whether to keep the interim svg file
+    """
+    inkscapePath = r"path\to\inkscape.exe"
+
+    figFolder = savePath + r"\{}.{}"
+    svgFile = figFolder.format(plotName,"svg")
+    emfFile = figFolder.format(plotName,"emf")
+    if fig:
+        use=fig
+    else:
+        use=plt
+    use.savefig(svgFile)
+    subprocess.run([inkscapePath, svgFile, '-M', emfFile])
+
+    if not keepSVG:
+        os.system('del "{}"'.format(svgFile))
